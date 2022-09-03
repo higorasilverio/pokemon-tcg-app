01bf0c1e02031dd4d9d4c9de5eecc2eda59b0bd6
higorasilverio <higorasilverio@outlook.com>
2022-09-03 17:02:14 -0300

    ♻️ (Card Model): some other properties added

--- src/modules/models/Card.ts
+++ src/modules/models/Card.ts
@@ -1,9 +1,25 @@
+type FightProperty = {
+  type: string
+  value: string
+}
+
+type AttackProperties = {
+  name: string
+  convertedEnergyCost: number
+  damage: string
+  text: string
+  cost: string[]
+}
+
 export class Card {
   #id: string
   #name: string
   #types: string[]
   #ldImageUrl: string
   #hdImageUrl: string
+  #resistances: FightProperty[]
+  #weaknesses: FightProperty[]
+  #attacks: AttackProperties[]
 
   constructor(json: any) {
     this.#id = json.id
@@ -11,6 +27,13 @@ export class Card {
     this.#types = json.types
     this.#ldImageUrl = json.images.small
     this.#hdImageUrl = json.images.large
+    this.#resistances = json.resistances
+    this.#weaknesses = json.weaknesses
+    this.#attacks = json.attacks
+  }
+
+  static emptyCard() {
+    return new Card({})
   }
 
   get id() {
@@ -31,5 +54,17 @@ export class Card {
 
   get hdImageUrl() {
     return this.#hdImageUrl
+  }
+
+  get resistances() {
+    return this.#resistances
+  }
+
+  get weaknesses() {
+    return this.#weaknesses
+  }
+
+  get attacks() {
+    return this.#attacks
   }
 }
